46f40ef12c5734aa5eff756926d58907b0f05b9a
laza88 <lazaryandrei@gmail.com>
2020-05-21 11:29:17 +0300

    added changes so dropdown-multi will not close when clicking on scrollbar

--- etools-dropdown-multi.js
+++ etools-dropdown-multi.js
@@ -74,7 +74,7 @@ class EtoolsDropdownMulti extends MultiDropdownRequiredMixins {
       <iron-dropdown id="dropdownMenu" horizontal-align="[[horizontalAlign]]" vertical-offset="[[verticalOffset]]"
                      dynamic-align="[[!noDynamicAlign]]" on-iron-overlay-opened="_onDropdownOpen"
                      on-iron-overlay-closed="_onDropdownClose" disabled="[[_menuBtnIsDisabled(disabled, readonly)]]"
-                     no-cancel-on-outside-click allow-click-through>
+                     no-cancel-on-outside-click allow-click-through with-backdrop="[[withBackdrop]]">
 
         <div id="ironDrContent" class="paper-material" elevation="1" slot="dropdown-content">
           <div id="dropdown-controls">
@@ -123,6 +123,15 @@ class EtoolsDropdownMulti extends MultiDropdownRequiredMixins {
         type: String,
         computed: '_getElementTitle(selectedItems)',
         reflectToAttribute: true
+      },
+      /* withBackdrop property was added in order to trap the focus within the light DOM of the iron-dropdown.
+         Setting this to true solves a bug in PRP where when you have the etools-dropdown in a paper-dialog,
+         and you click on the opened drodpdown's scroll,  the dropdown closes.
+      **/
+      withBackdrop: {
+        type: Boolean,
+        reflectToAttribute: true,
+        value: false
       }
     };
   }
