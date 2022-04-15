cbff9535ecc989eab192e96a463a6f40f0096daf
dcioara <dan.cioara@nordlogic.com>
2022-04-15 13:40:51 +0300

    updated with develop

--- EtoolsDropdown.js
+++ EtoolsDropdown.js
@@ -162,7 +162,13 @@ export class EtoolsDropdown extends DropdownRequiredMixins {
         allow-click-through
         with-backdrop="[[withBackdrop]]"
       >
-        <div id="ironDrContent" class="paper-material rounded" elevation="1" slot="dropdown-content">
+        <div
+          id="ironDrContent"
+          class="paper-material rounded"
+          elevation="1"
+          slot="dropdown-content"
+          part="esmm-dropdown-content"
+        >
           <esmm-searchbox-input
             id="searchbox"
             search="[[search]]"
