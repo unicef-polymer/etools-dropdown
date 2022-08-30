595d193a721f22d3708bfc0961097671f481fe91
Adriana Trif <trif.corina.adriana@gmail.com>
2022-08-30 23:32:20 +0300

    Rename, move file

---
+++ assets/translations.js
@@ -0,0 +1,21 @@
+const translations = {
+  en: {
+    SEARCH: 'Search',
+    CLOSE: 'Close',
+    NONE: 'None',
+    NO_OPTIONS_AVAILABLE: 'No options available.',
+    SCROLL_DOWN_TO_REVEAL_MORE_ITEMS: 'Scroll down to reveal more items.',
+    REQUEST_IN_PROGRESS: 'Request in progress.',
+    NO_RESULTS_FOUND_TRY_OTHER_KEYWORDS: 'No results found. Try other keywords.'
+  },
+  fr: {
+    SEARCH: 'Chercher',
+    CLOSE: 'Fermer',
+    NONE: 'Rien',
+    NO_OPTIONS_AVAILABLE: 'Aucune option disponible.',
+    SCROLL_DOWN_TO_REVEAL_MORE_ITEMS: "Défiler vers le bas pour révéler plus d'éléments.",
+    REQUEST_IN_PROGRESS: 'Demande en cours.',
+    NO_RESULTS_FOUND_TRY_OTHER_KEYWORDS: "Aucun résultat trouvé. Essayez d'autres mots clés."
+  }
+};
+export default translations;
