8172142fec0ed8244bf4fb69234d48e39b20d1ba
Trif Adriana <trif.corina.adriana@gmail.com>
2022-09-02 22:37:20 +0300

    Add ro,pt,ru translations

--- assets/translations.js
+++ assets/translations.js
@@ -16,6 +16,33 @@ const translations = {
     SCROLL_DOWN_TO_REVEAL_MORE_ITEMS: "Défiler vers le bas pour révéler plus d'éléments.",
     REQUEST_IN_PROGRESS: 'Demande en cours.',
     NO_RESULTS_FOUND_TRY_OTHER_KEYWORDS: "Aucun résultat trouvé. Essayez d'autres mots clés."
+  },
+  ro: {
+    SEARCH: 'Cauta',
+    CLOSE: 'Inchide',
+    NONE: 'Nici unul',
+    NO_OPTIONS_AVAILABLE: 'Nu sunt optiuni disonibile.',
+    SCROLL_DOWN_TO_REVEAL_MORE_ITEMS: 'Scroll in jos pt a vedea mai mult.',
+    REQUEST_IN_PROGRESS: 'Cerere in procesare.',
+    NO_RESULTS_FOUND_TRY_OTHER_KEYWORDS: 'Nu sunt rezultate. Incearca alte cuvinte cheie.'
+  },
+  pt: {
+    SEARCH: 'Procurar',
+    CLOSE: 'Fechar',
+    NONE: 'Nenhum',
+    NO_OPTIONS_AVAILABLE: 'Nenhuma opção disponível.',
+    SCROLL_DOWN_TO_REVEAL_MORE_ITEMS: 'Role para baixo para revelar mais itens.',
+    REQUEST_IN_PROGRESS: 'Solicitação em andamento.',
+    NO_RESULTS_FOUND_TRY_OTHER_KEYWORDS: 'Nenhum resultado encontrado. Tente outras palavras-chave.'
+  },
+  ru: {
+    SEARCH: 'поиск',
+    CLOSE: 'Закрыть',
+    NONE: 'никто',
+    NO_OPTIONS_AVAILABLE: 'Нет доступных вариантов.',
+    SCROLL_DOWN_TO_REVEAL_MORE_ITEMS: 'прокрутите вниз, чтобы открыть больше элементов.',
+    REQUEST_IN_PROGRESS: 'Выполняется запрос.',
+    NO_RESULTS_FOUND_TRY_OTHER_KEYWORDS: 'Результатов не найдено. Попробуйте другие ключевые слова.'
   }
 };
 export default translations;
